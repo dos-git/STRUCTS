44b337e3cff2351e05375e8f283901d1166e71b0
Dominik <dominik.stefanowicz@gmail.com>
2015-12-25 16:08:42 +0100

    dummy commit

--- main.c
+++ main.c
@@ -8,22 +8,31 @@
 
 int main(int argc, char *argv[])
 {
+    char *l = "SD";
     char *n = "DOMINO";
     char *n_2 = "STEFANO";
+    char *n_3;
+    char a[10];
+    char b[] = "PPP";
+    printf("LEN 0     : %d\n", strlen(l));
+    printf("LEN 1     : %d\n", strlen(n));
+    printf("LEN 2     : %d\n", strlen(n_2));
     basic_struct_t bs;
+    a = (char*)b;
+    //n_3 = "ds";
+    //
+
     bs.nr = 1;
-    printf("LEN : %d\n", sizeof(n));
-    strncpy(bs.name, n, sizeof(n));
+    printf("LEN FIXED ARRAY : %d\n", sizeof(bs.name_fixed_array));
+    printf("LEN POINTER     : %d\n", sizeof(bs.name_pointer));
+    bs.name_pointer = n_3;
+    strncpy(bs.name_fixed_array, n, sizeof(n));
+    bs.name_pointer = (char *)malloc(sizeof(n_2));
+    strncpy(bs.name_pointer, n_2, sizeof(n_2));
 
-    printf("LEN2 : %d\n", sizeof(n_2));
-    bs.name_2 = (char *)malloc(sizeof(n_2));
-    strncpy(bs.name_2, n_2, sizeof(n_2));
-
-
-    printf("NUMBER : %d\n", bs.nr);
-    printf("NAME   : %s\n", bs.name);
-    printf("NAME2  : %s\n", bs.name_2);
-    free(bs.name_2);
-
+    printf("LEN FIXED ARRAY : %d\n", sizeof(bs.name_fixed_array));
+    printf("LEN POINTER     : %d\n", sizeof(bs.name_pointer));
+    free(bs.name_pointer);
+    printf("DUPA\n");
     return 0;
 }
